5851b42feda8386e7a1b43edf7d06c5926738187
Olena Olishevska <retyna9001@gmail.com>
2020-02-29 22:56:16 +0200

    lab

---
+++ task1_varA/ConsoleApp1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1a_multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first signed number");
+            int multiplicand = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second signed number");
+            int multiplier = int.Parse(Console.ReadLine());
+            Evaluate(multiplicand, multiplier);
+            Console.ReadKey();
+        }
+
+        public static void Evaluate(int multiplicand, int multiplier)
+        {
+            Int64 P = 0;
+            bool same_signs = true;
+            string A_bits = ToBinary(multiplicand), B_bits = ToBinary(multiplier);
+            Console.WriteLine("\tMultiplicand: {0}", A_bits);
+            Console.WriteLine("\tMultiplier: {0}", B_bits);
+
+            if (multiplicand * multiplier < 0)
+            {
+                same_signs = false;
+            }
+
+            if (multiplicand < 0)
+            {
+                multiplicand = ~multiplicand + 1;
+                Console.WriteLine("\nMultiplicand is negative number: we'll work with complement code");
+                A_bits = ToBinary(multiplicand);
+                Console.WriteLine("Multiplicand:\t" + A_bits);
+            }
+
+            if (multiplier < 0)
+            {
+                multiplier = ~multiplier + 1;
+                Console.WriteLine("\nMultiplicand is negative number: we'll work with complement code");
+                B_bits = ToBinary(multiplier);
+                Console.WriteLine("Multiplier:\t" + B_bits);
+            }
+
+            Console.WriteLine("Multiply as is:");
+            for (int i = 1; i < 17; ++i)
+            {
+                Console.WriteLine("  Step " + i + ":");
+                if ((multiplier & 1) == 1)
+                {
+                    Console.WriteLine("  \tAdd Multiplicand:\t{0}\n\tTo Product:\t\t{1}", A_bits, ToBinary(P));
+                    P += multiplicand;
+                }
+                Console.WriteLine("  \tProduct :\t\t{0}", ToBinary(P));
+
+                Console.WriteLine("  \tShift Multiplicand left:" + ToBinary(multiplicand));
+                multiplicand <<= 1;
+                Console.WriteLine("  \t\t\t\t" + ToBinary(multiplicand));
+
+                Console.WriteLine("  \tShift Multiplier right:\t" + ToBinary(multiplier));
+                multiplier >>= 1;
+                Console.WriteLine("  \t\t\t " + ToBinary(multiplier));
+            }
+            if (!same_signs)
+            {
+                P = ~P + 1;
+                Console.WriteLine("\n  Result is complement code, because our multiplicands have different sign");
+            }
+            Console.WriteLine("  Answer is:\n\tIn decemal: {0}\n\tIn binary: {1}", P, ToBinary(P));
+
+
+
+
+
+
+        }
+
+        public static string ToBinary(Int64 num)
+        {
+            string binary = string.Empty;
+            for (int i = 1; i < 33; ++i)
+            {
+                binary = (i % 4 == 0 ? " " : "") + (num & 1) + binary;
+                num >>= 1;
+            }
+            return binary;
+        }
+    }
+}
---
+++ task2_varA/ConsoleApp1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2a_dividing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sign divident");
+            int divident = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the sign divisor");
+            int divisor = int.Parse(Console.ReadLine());
+            Divide(divident, divisor);
+            Console.ReadKey();
+
+        }
+
+        static void Divide(int divident, int divisor)
+        {
+
+
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            var def = string.Empty;
+            var dividentAbs = Math.Abs(divident);
+            var divisorAbs = Math.Abs(divisor);
+            int remainder = 0, quotient = 0;
+
+            if (dividentAbs < divisorAbs)
+            {
+                quotient = divident;
+                def += "Divident < Divisor, so Remainder = Divident , Quotient = 0\n";
+            }
+            else
+            {
+                int lenA, lenB, k;
+                int temporal = dividentAbs;
+
+                def += $"{Convert.ToString(dividentAbs, 2)} mod Divident\n";
+                def += $"{Convert.ToString(divisorAbs, 2)} mod Divisor\n\n";
+
+                for (lenA = 0; temporal != 0; temporal >>= 1, lenA++) ;
+                temporal = divisorAbs;
+                for (lenB = 0; temporal != 0; temporal >>= 1, lenB++) ;
+                k = lenA - lenB;
+                divisorAbs <<= k;
+
+                def += $"Divisor align\n";
+                def += $"{Convert.ToString(dividentAbs, 2)} mod Divident\n";
+                def += $"{Convert.ToString(divisorAbs, 2)} mod Divisor\n\n";
+                quotient = dividentAbs + -divisorAbs;
+                remainder = quotient < 0 ? 0 : 1;
+                def += $"Add Divident & Divisor in additional code\n";
+                def += $"Quotient = {Convert.ToString(quotient, 2)} \n";
+                def += $"Remainder = {Convert.ToString(remainder, 2)} \n\n";
+                for (int i = 0; i < k; i++)
+                {
+                    divisorAbs >>= 1;
+                    def += $"{Convert.ToString(divisorAbs, 2)}  Divisor Right Shift\n";
+                    if (quotient < 0)
+                    {
+                        quotient += divisorAbs;
+                        def += $"{Convert.ToString(quotient, 2)} Sum Qoutient & Divisor\n";
+                    }
+                    else
+                    {
+                        quotient += -divisorAbs;
+                        def += $"{Convert.ToString(quotient, 2)} Sub Qoutient & Divisor\n";
+                    }
+                    remainder <<= 1;
+                    if (quotient >= 0)
+                    {
+                        remainder++;
+                    }
+
+                    def += $"{Convert.ToString(remainder, 2)} Remainder Left Shift {(quotient >= 0 ? "& add 1" : "")}\n\n";
+                }
+                if (quotient < 0)
+                {
+                    def += "Determine Qoutient\n";
+                    quotient += divisorAbs;
+                    def += $"{Convert.ToString(quotient, 2)} Qoutient += |Divisor| \n\n";
+                }
+                def += "Analyze sign bit of Dividend & Divisor and set sign bit to Remainder and Quotient\n";
+                if (divident < 0)
+                {
+                    if (divisor > 0)
+                    {
+                        remainder = -remainder;
+                    }
+
+                    quotient = -quotient;
+                }
+                if (divident > 0 && divisor < 0)
+                {
+                    remainder = -remainder;
+                }
+
+                def += $"Remainder = {Convert.ToString(remainder, 2)} Quotient = {Convert.ToString(quotient, 2)}\n";
+            }
+
+            Console.WriteLine("def = {0}", def);
+            Console.WriteLine("Remainder = {0}, Quotient = {1}", remainder, quotient);
+        }
+    }
+}
